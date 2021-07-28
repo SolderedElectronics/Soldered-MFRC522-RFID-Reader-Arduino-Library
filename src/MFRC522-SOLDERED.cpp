403ae7146de16d5047f00905f76e839a9b790eec
nitko12 <nitko12@hotmail.com>
2021-07-28 12:08:42 +0200

    Final

---
+++ src/MFRC522-SOLDERED.cpp
@@ -0,0 +1,13 @@
+/**
+ **************************************************
+ *
+ * @file        MFRC522-SOLDERED.cpp
+ * @brief       Blank because no new code added.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Zvonimir Haramustek for soldered.com
+ ***************************************************/
+
+
+// #include "MFRC522-SOLDERED.h"
