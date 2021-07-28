4e8210ac3b35f5fa90492d63e076fa521f7dc05f
Main Push Robot <robot@example.com>
2021-07-28 10:17:08 +0000

    Committing dev changes

--- examples/MifareClassicValueBlock/MifareClassicValueBlock.ino
+++ examples/MifareClassicValueBlock/MifareClassicValueBlock.ino
@@ -325,7 +325,8 @@ void formatValueBlock(byte blockAddr)
     else
     {
         Serial.println(F("Formatting as Value Block..."));
-        byte valueBlock[] = {0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, blockAddr, ~blockAddr, blockAddr, ~blockAddr};
+        byte valueBlock[] = {
+            0, 0, 0, 0, 255, 255, 255, 255, 0, 0, 0, 0, blockAddr, ~(byte)blockAddr, blockAddr, ~(byte)blockAddr};
         status = mfrc522.MIFARE_Write(blockAddr, valueBlock, 16);
         if (status != MFRC522::STATUS_OK)
         {
