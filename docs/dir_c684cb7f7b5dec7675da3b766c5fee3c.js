5fe16c2fc5597a3ccbd3f9c713d20b000309f7f2
Docs Robot <robot@example.com>
2023-02-14 15:36:52 +0000

    Committing documentation changes

--- docs/dir_c684cb7f7b5dec7675da3b766c5fee3c.js
+++ docs/dir_c684cb7f7b5dec7675da3b766c5fee3c.js
@@ -4,11 +4,6 @@ var dir_c684cb7f7b5dec7675da3b766c5fee3c =
     [ "MFRC522.cpp", "_m_f_r_c522_8cpp.html", null ],
     [ "MFRC522.h", "_m_f_r_c522_8h.html", "_m_f_r_c522_8h" ],
     [ "MFRC522Extended.cpp", "_m_f_r_c522_extended_8cpp.html", null ],
-    [ "MFRC522Extended.h", "_m_f_r_c522_extended_8h.html", [
-      [ "MFRC522Extended", "class_m_f_r_c522_extended.html", "class_m_f_r_c522_extended" ],
-      [ "MFRC522Extended::Ats", "struct_m_f_r_c522_extended_1_1_ats.html", "struct_m_f_r_c522_extended_1_1_ats" ],
-      [ "MFRC522Extended::TagInfo", "struct_m_f_r_c522_extended_1_1_tag_info.html", "struct_m_f_r_c522_extended_1_1_tag_info" ],
-      [ "MFRC522Extended::PcbBlock", "struct_m_f_r_c522_extended_1_1_pcb_block.html", "struct_m_f_r_c522_extended_1_1_pcb_block" ]
-    ] ],
+    [ "MFRC522Extended.h", "_m_f_r_c522_extended_8h.html", "_m_f_r_c522_extended_8h" ],
     [ "require_cpp11.h", "require__cpp11_8h.html", null ]
 ];
